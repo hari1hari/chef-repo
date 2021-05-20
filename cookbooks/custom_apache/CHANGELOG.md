fa7ac84228f008e30d86613a133e2ab88aca5220
root <root@chefworkstation.example.com>
2021-05-20 21:16:03 +0530

    ADD

---
+++ cookbooks/custom_apache/CHANGELOG.md
@@ -0,0 +1,11 @@
+# custom_apache CHANGELOG
+
+This file is used to list changes made in each version of the custom_apache cookbook.
+
+# 0.1.0
+
+Initial release.
+
+- change 0
+- change 1
+
---
+++ cookbooks/custom_apache/recipes/default.rb
@@ -0,0 +1,16 @@
+#
+# Cookbook:: custom_apache
+# Recipe:: default
+#
+# Copyright:: 2021, The Authors, All Rights Reserved.
+package 'httpd' do
+action: install
+end
+cookbook_file '/var/www/html/index.html' do
+source 'index.html.raw'
+mode 0644
+end
+
+service 'httpd' do
+action [:enable, :start]
+end
