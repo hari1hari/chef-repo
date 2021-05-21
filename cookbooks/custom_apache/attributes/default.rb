45a292b27c23ee86f91fc55952501d2fd66fa629
root <root@chefworkstation.example.com>
2021-05-21 15:50:25 +0530

    new version 2.0

---
+++ cookbooks/custom_apache/attributes/default.rb
@@ -0,0 +1,4 @@
+default['apache']['url'] = "https://www.free-css.com/assets/files/free-css-templates/download/page267/studio-francesca.zip"
+default['apache']['user'] = 'root'
+default['apache']['group'] = 'root'
+default['apache']['path'] = '/var/www/html'
--- cookbooks/custom_apache/recipes/default.rb
+++ cookbooks/custom_apache/recipes/default.rb
@@ -12,6 +12,19 @@ source 'index.html.raw'
 mode 0644
 end
 
+ark 'template' do
+url node['apache']['url']
+prefix_root node['apache']['path']
+owner node['apache']['user']
+group node['apache']['group']
+
+notifies :restart, 'service[httpd]', :delayed
+
+
 service 'httpd' do
 action [:enable, :start]
+end
+
+package 'unzip' do
+action :install
 end
